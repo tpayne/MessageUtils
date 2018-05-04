4853ee681b2804ea8811ed54145b0337fff356df
Tim Payne <tpayne@serena.com>
2018-05-04 11:04:12 +0100

    Delete files

--- run.bat
+++
@@ -1,1 +0,0 @@
-win32x86_debug\MessengerUtils.exe messenger.hotmail.com 1863 -user hitomivarn@hotmail.com -password evangelion --debug 
--- run.sh
+++
@@ -1,1 +0,0 @@
-./Mess* messenger.hotmail.com 1863 -user hitomivarn@hotmail.com -password evangelion --debug
